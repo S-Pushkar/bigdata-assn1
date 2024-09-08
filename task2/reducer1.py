a875bbe96ec5d11edc2c4062edcc2ee4743c2874
Pushkar S <spushkar0071234@gmail.com>
2024-09-09 00:21:15 +0530

    working (brute force) moved logic to reducer1

--- task2/reducer1.py
+++ task2/reducer1.py
@@ -2,6 +2,99 @@
 
 import sys
 
+servers = {
+    "user/profile": -1,
+    "user/settings": -1,
+    "order/history": -1,
+    "order/checkout": -1,
+    "product/details": -1,
+    "product/search": -1,
+    "cart/add": -1,
+    "cart/remove": -1,
+    "payment/submit": -1,
+    "support/ticket": -1
+}
+
+previous_timestamp = ''
+
+clients_in_current_timestamp = set()
+
+endpoints = set(servers.keys())
+
 for l in sys.stdin:
     line = l.strip()
-    print(line)
+    line_array = line.split(',')
+    if len(line_array) == 2:
+        print(line)
+    elif len(line_array) == 5:
+        timestamp = line_array[0]
+        client_id = line_array[2]
+        request_id = line_array[1]
+        endpoint = line_array[3]
+        no_of_servers_down = int(line_array[4])
+        if timestamp == previous_timestamp:
+            if client_id not in clients_in_current_timestamp:
+                if no_of_servers_down == 3:
+                    servers[endpoint] = 0
+                    clients_in_current_timestamp.add(client_id)
+                    print(f"{timestamp},{client_id},{request_id},{endpoint},{no_of_servers_down},500")
+                elif servers[endpoint] == -1:
+                    servers[endpoint] = 2 - no_of_servers_down
+                    clients_in_current_timestamp.add(client_id)
+                    print(f"{timestamp},{client_id},{request_id},{endpoint},{no_of_servers_down},200")
+                elif servers[endpoint] > 0:
+                    servers[endpoint] -= 1
+                    clients_in_current_timestamp.add(client_id)
+                    print(f"{timestamp},{client_id},{request_id},{endpoint},{no_of_servers_down},200")
+                elif servers[endpoint] <= 0:
+                    clients_in_current_timestamp.add(client_id)
+                    print(f"{timestamp},{client_id},{request_id},{endpoint},{no_of_servers_down},500")
+        else:
+            previous_timestamp = timestamp
+            for server in servers.keys():
+                servers[server] = -1
+            clients_in_current_timestamp.clear()
+            if no_of_servers_down == 3:
+                servers[endpoint] = 0
+                clients_in_current_timestamp.add(client_id)
+                print(f"{timestamp},{client_id},{request_id},{endpoint},{no_of_servers_down},500")
+                continue
+            servers[endpoint] = 2 - no_of_servers_down
+            clients_in_current_timestamp.add(client_id)
+            print(f"{timestamp},{client_id},{request_id},{endpoint},{no_of_servers_down},200")
+        # if timestamp == previous_timestamp and client_id in clients_in_current_timestamp:
+        #     continue
+        # # if no_of_servers_down == 3:
+        # #     print(f"{timestamp},{client_id},{request_id},{endpoint},{no_of_servers_down},500")
+        # else:
+        #     if timestamp == previous_timestamp:
+        #         if client_id not in clients_in_current_timestamp:
+        #             if no_of_servers_down == 3:
+        #                 servers[endpoint] = 0
+        #                 clients_in_current_timestamp.add(client_id)
+        #                 print(f"{timestamp},{client_id},{request_id},{endpoint},{no_of_servers_down},500")
+        #             if servers[endpoint] == -1:
+        #                 servers[endpoint] = 2 - no_of_servers_down
+        #                 clients_in_current_timestamp.add(client_id)
+        #                 print(f"{timestamp},{client_id},{request_id},{endpoint},{no_of_servers_down},200")
+        #             elif servers[endpoint] > 0:
+        #                 servers[endpoint] -= 1
+        #                 clients_in_current_timestamp.add(client_id)
+        #                 print(f"{timestamp},{client_id},{request_id},{endpoint},{no_of_servers_down},200")
+        #             else:
+        #                 clients_in_current_timestamp.add(client_id)
+        #                 print(f"{timestamp},{client_id},{request_id},{endpoint},{no_of_servers_down},500")
+        #     else:
+        #         previous_timestamp = timestamp
+        #         for server in servers.keys():
+        #             servers[server] = -1
+        #         # previous_timestamp = timestamp
+        #         clients_in_current_timestamp.clear()
+        #         if no_of_servers_down == 3:
+        #             servers[endpoint] = 0
+        #             clients_in_current_timestamp.add(client_id)
+        #             print(f"{timestamp},{client_id},{request_id},{endpoint},{no_of_servers_down},500")
+        #             continue
+        #         servers[endpoint] = 2 - no_of_servers_down
+        #         clients_in_current_timestamp.add(client_id)
+        #         print(f"{timestamp},{client_id},{request_id},{endpoint},{no_of_servers_down},200")
--- task2/reducer3.py
+++ task2/reducer3.py
@@ -8,7 +8,7 @@ previous_request_id = None
 
 clients = {}
 
-# requests = {}
+requests = {}
 
 for l in sys.stdin:
     line = l.strip()
@@ -16,7 +16,7 @@ for l in sys.stdin:
     if len(line_array) == 2:
         previous_request_id = line_array[0]
         prediction = int(line_array[1])
-        # requests[line_array[0]] = prediction
+        requests[line_array[0]] = prediction
     elif len(line_array) == 4:
         request_id = line_array[0]
         client_id = line_array[1]
@@ -26,12 +26,12 @@ for l in sys.stdin:
             clients[client_id] = {"total_cost": 0, "total_requests": 0, "number_of_correct_predictions": 0}
         clients[client_id]["total_cost"] += cost
         clients[client_id]["total_requests"] += 1
-        # if request_id in requests:
-        #     if status_code == requests[request_id]:
-        #         clients[client_id]["number_of_correct_predictions"] += 1
-        if previous_request_id == request_id:
-            if status_code == prediction:
+        if request_id in requests:
+            if status_code == requests[request_id]:
                 clients[client_id]["number_of_correct_predictions"] += 1
+        # if previous_request_id == request_id:
+        #     if status_code == prediction:
+        #         clients[client_id]["number_of_correct_predictions"] += 1
 
 client_keys = sorted(clients.keys())
 for client_id in client_keys:
